85faaefe7efc124c68268665ce0abb19e953329b
Jonathan Bidwell <jbidwellb@gmail.com>
2022-07-03 14:05:33 -0400

    entrar

---
+++ 1 funcion.js
@@ -0,0 +1,9 @@
+//declaración de función
+function example(a, b, c) {
+    return a + b + c;
+}
+
+//expresión de función
+example = function (a, b, c) {
+    return a + b + c;
+};
---
+++ 2 arrow.js
@@ -0,0 +1,7 @@
+//expresión de función
+suma = function (a, b) {
+    return a + b;
+};
+
+//arrow function
+suma = (a, b) => a + b;//declaración de función
